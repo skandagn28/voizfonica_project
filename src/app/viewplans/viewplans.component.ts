578f6a426928fa36b14e4d9a22e0fd746f91bd1e
Skanda Gurunathan R <sgnmail01@gmail.com>
2019-08-06 02:12:11 +0530

    view plans done

--- src/app/viewplans/viewplans.component.ts
+++ src/app/viewplans/viewplans.component.ts
@@ -1,21 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
+import { MatDialog, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { ApiService } from "../api.service";
 
+export interface DialogData {
+  animal: "panda" | "unicorn" | "lion";
+}
 @Component({
-  selector: 'app-viewplans',
-  templateUrl: './viewplans.component.html',
-  styleUrls: ['./viewplans.component.scss']
+  selector: "dialog-data-example",
+  templateUrl: "viewcosting.html"
 })
-export class ViewplansComponent implements OnInit {
-
-  selectedpane: number;
-  constructor() { }
-
-  ngOnInit() {
-    this.selectedpane=1;
-  }
-  changepane(pane: number){
-   
-    this.selectedpane = pane;
+export class DialogDataExampleDialog {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
 }
 
+@Component({
+  selector: "app-viewplans",
+  templateUrl: "./viewplans.component.html",
+  styleUrls: ["./viewplans.component.scss"]
+})
+export class ViewplansComponent implements OnInit {
+  selectedpane: number;
+
+  plans = [];
+
+  constructor(public dialog: MatDialog, public apiService: ApiService) {}
+
+  ngOnInit() {
+    this.selectedpane = 2;
+    this.apiService.getPlans().subscribe(data => {
+      this.plans = data;
+    });
+  }
+  changepane(pane: number) {
+    this.selectedpane = pane;
+  }
+
+  openDialog(l, r, i) {
+    this.dialog.open(DialogDataExampleDialog, {
+      width: "350px",
+      data: {
+        l: l,
+        r: r,
+        i: i
+      }
+    });
+  }
 }
